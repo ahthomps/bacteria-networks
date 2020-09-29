b18a04abbdd0392e8c06d8b005fe5d980a91ea01
bkallus@hamilton.edu <bkallus@hamilton.edu>
2020-09-28 23:00:40 -0400

    now it runs, but it crops things totally wrong

--- make_labeled_crops.py
+++ make_labeled_crops.py
@@ -24,6 +24,9 @@ class Tile(Box):
         # (i.e. if a bounding box starts on the left edge of this tile, its x1 is 0)
         self.bounding_boxes = []
 
+        # This will be used as a unique identifier for this crop.
+        self.filename = f"{self.x1}_{self.x2}"
+
     def add_bounding_box(self, box):
         box = deepcopy(box)
         box.x1 = max(box.x1, self.x1) - self.x1
@@ -33,12 +36,12 @@ class Tile(Box):
         self.bounding_boxes.append(box)
 
     def to_relative_bounding_boxes(self):
-        for box in boxes:
-            box.to_relative()
+        for box in self.bounding_boxes:
+            box.to_relative(TILE_SIZE, TILE_SIZE)
 
-    def save(directory=".", filename=f"{self.x1}_{self.y1}"):
-        crop.save(f"{directory}/{filename}.jpg", "JPEG", subsampling=0, quality=100)
-        ofile = open(f"{directory}/{filename}.txt", "w")
+    def save(self, directory="."):
+        self.crop.save(f"{directory}/{self.filename}.jpg", "JPEG", subsampling=0, quality=100)
+        ofile = open(f"{directory}/{self.filename}.txt", "w")
         for box in self.bounding_boxes:
             ofile.write(f"{box.classification} {box.x1} {box.y1} {box.x2} {box.y2}\n")
         ofile.close()
@@ -67,29 +70,29 @@ class BoundingBox(Box):
         self.y2 /= height
         self.in_px = False
 
-    def overlaps(box):
+    def overlaps(self, box):
         assert self.in_px
         return any(box.x1 < x < box.x2 and box.y1 < y < box.y2 \
-           for point in [(self.x1,self.y1), (self.x1,self.y2), (self.x2,self.y1), (self.x2,self.y2)])
+           for (x,y) in [(self.x1,self.y1), (self.x1,self.y2), (self.x2,self.y1), (self.x2,self.y2)])
         
 
 
-def parse_yolo_input(label_file, img):
+def parse_yolo_input(label_file):
     """ Reads from a yolo training file and returns a list of BoundingBox objects.
         Also takes the labels' image so we can convert from relative to px. """
     bounding_boxes = []
-    for line in file.readlines():
+    for line in label_file.readlines():
         # Ignore comments
         if "#" in line:
             line = line[:line.index("#")]
         if line.split() == []:
             continue
 
-        bounding_boxes.append(BoundingBox(*map(int, line.split())))
+        bounding_boxes.append(BoundingBox(*map(float, line.split())))
 
     return bounding_boxes
 
-def main(iamge_filename, label_filename, output_dir):
+def main(image_filename, label_filename, output_dir):
     im = Image.open(image_filename)
     labels = open(label_filename)
     
@@ -112,7 +115,7 @@ def main(iamge_filename, label_filename, output_dir):
 
     for tile in tiles:
         tile.to_relative_bounding_boxes()
-
+        tile.save(output_dir)
 
 
 if __name__ == "__main__":
@@ -120,7 +123,7 @@ if __name__ == "__main__":
         print("USAGE: python3 parse_yolo_file.py <image_file> <label_file> <output_directory>", file=sys.stderr)
         sys.exit(1)
 
-    main(*argv[1:])
+    main(*sys.argv[1:])
 
 # TO DO:
 # Batch processing
