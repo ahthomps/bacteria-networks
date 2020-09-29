a0b1b553b733e2bf90a4cef657b93b208e7ea5e6
Ben Kallus <bkallus@hamilton.edu>
2020-09-28 22:26:09 -0400

    worked on tiling script

---
+++ make_labeled_crops.py
@@ -0,0 +1,118 @@
+import sys
+from PIL import Image
+from copy import deepcopy
+
+TILE_OVERLAP = 2 # 2 -> 50% overlap, 3 -> 33% overlap, etc.
+TILE_SIZE = 416
+
+class Box:
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+class Tile(Box):
+    def __init__(self, crop, x1, y1, x2, y2):
+        """ crop:           The cropped PIL Image object.
+            x1, y1, x2, y2: The position of this tile in the larger image. """
+        super().__init__(x1, y1, x2, y2)
+        self.crop = crop
+
+        # This will store all the (potentially partial) bounding boxes that overlap this tile.
+        # Their coordinates are relative to this tile.
+        # (i.e. if a bounding box starts on the left edge of this tile, its x1 is 0)
+        self.bounding_boxes = []
+
+    def add_bounding_box(self, box):
+        box = deepcopy(box)
+        box.x1 = max(box.x1, self.x1) - self.x1
+        box.y1 = max(box.y1, self.y1) - self.y1
+        box.x2 = min(box.x2, self.x2) - self.x2
+        box.y2 = min(box.y2, self.y2) - self.y2
+        self.bounding_boxes.append(box)
+
+    def to_relative_bounding_boxes(self):
+        for box in boxes:
+            box.to_relative()
+
+
+class BoundingBox(Box):
+    def __init__(self, classification, x1, y1, x2, y2):
+        """ classification: A number representing a YOLO classification
+            x1, y1, x2, y2: Floats between 0 and 1 representing a relative position. """
+        super().__init__(x1, y1, x2, y2)
+        self.classification = classification
+        self.in_px = False
+
+    def to_px(self, width, height):
+        self.x1 *= width
+        self.x2 *= width
+        self.y1 *= height
+        self.y2 *= height
+        self.in_px = True
+
+    def to_relative(self, width, height):
+        self.x1 /= width
+        self.x2 /= width
+        self.y1 /= height
+        self.y2 /= height
+        self.in_px = False
+
+    def overlaps(box):
+        assert self.in_px
+        return any(box.x1 < x < box.x2 and box.y1 < y < box.y2 \
+           for point in [(self.x1,self.y1), (self.x1,self.y2), (self.x2,self.y1), (self.x2,self.y2)])
+        
+
+
+def parse_yolo_input(label_file, img):
+    """ Reads from a yolo training file and returns a list of BoundingBox objects.
+        Also takes the labels' image so we can convert from relative to px. """
+    bounding_boxes = []
+    for line in file.readlines():
+        # Ignore comments
+        if "#" in line:
+            line = line[:line.index("#")]
+        if line.split() == []:
+            continue
+
+        bounding_boxes.append(BoundingBox(*map(int, line.split())))
+
+    return bounding_boxes
+
+def main():
+    if len(sys.argv) != 4:
+        print("USAGE: python3 parse_yolo_file.py <image_file> <label_file> <output_directory>", file=sys.stderr)
+        sys.exit(1)
+
+    image_filename = argv[1]
+    label_filename = argv[2]
+    output_dir = argv[3]
+
+    im = Image.open(image_filename)
+    labels = open(label_filename)
+    
+    # Make the BoundingBox objects
+    bounding_boxes = parse_yolo_input(labels)
+
+    # Convert them to px
+    for box in bounding_boxes:
+        box.to_px(im.width, im.height)
+
+    tiles = []
+    for r in range(0, im.width, TILE_SIZE // TILE_OVERLAP):
+        for c in range(0, im.height, TILE_SIZE // TILE_OVERLAP):
+            x1, y1, x2, y2 = (r, c, r + TILE_SIZE, c + TILE_SIZE)
+            tile = Tile(im.crop((x1, y1, x2, y2)), x1, y1, x2, y2)
+            for box in bounding_boxes:
+                if box.overlaps(tile):
+                    tile.add_bounding_box(box)
+            tiles.append(tile)
+
+    for tile in tiles:
+
+
+
+if __name__ == "__main__":
+    main()
