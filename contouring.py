8529682f72787b9380a8142d81a3b3ff89d3f7ce
ahthompson <almaotter@gmail.com>
2020-10-01 10:48:28 -0400

    revamped contouring

---
+++ contouring.py
@@ -0,0 +1,138 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from copy import deepcopy
+from skimage import morphology, filters, exposure, segmentation, restoration, color, util, measure
+from skimage.viewer import ImageViewer
+from scipy import ndimage as ndi
+import sys
+import math
+from classes import *
+
+def erode_and_dilate(image, erode, dilate):
+    """ Performs erosions and dialations on an image and returns processed
+        image."""
+    image_open = image
+    for _ in range(erode):
+        image_open = morphology.erosion(image_open)
+    for _ in range(dilate):
+        image_open = morphology.dilation(image_open)
+
+    return image_open
+
+def process_image(image, threshold=None, openings=None, initial_dilations=None):
+    """ Takes original image (np.array) and option of numbers for threshold and
+        number of openings. Returns an binary image (np.array type np.int8)"""
+
+    # converts from RBG to grayscale image
+    image_gray = color.rgb2gray(image)
+
+    # creates binary image
+    if not threshold:
+        # finds threshold using Yen technique
+        threshold = filters.threshold_yen(deepcopy(image_gray))
+    image_binary = image_gray > threshold
+
+    if not openings:
+        # default number of openings
+        openings = 7
+    if not initial_dilations:
+        # default number of inital dilations
+        initial_dilations = 4
+
+    # filling any in the cells -- seems to work better than the fill_holes method
+    image_binary = erode_and_dilate(image_binary, 0, initial_dilations)
+    image_binary = erode_and_dilate(image_binary, initial_dilations, 0)
+
+    # performs opening to reduce noise around cells
+    image_binary = erode_and_dilate(image_binary, openings, openings)
+
+    return image_binary.astype(np.int8)
+
+def get_bbox_overlaps(bounding_boxes):
+    """ Takes a list of Box objects and returns a list of the overlaps of each
+        box. The overlaps of bounding_boxes[i] are in overlaps[i]. """
+    overlaps = [[] for _ in range(len(bounding_boxes))]
+
+    for i in range(len(bounding_boxes) - 1):
+        box1 = bounding_boxes[i]
+        for j in range(i + 1, len(bounding_boxes)):
+            box2 = bounding_boxes[j]
+
+            # note: if there exists some box1 that is totally inside a box2,
+            #       overlaps[box1] does NOT include box2 -- I chose to do this
+            #       because when looking for balloon start I don't want to
+            #       subtract all of box1 (what I'm looking for)
+
+            # A.overlaps(B) finds the number of corners of A that are in B
+            points_in_box2 = box1.overlaps(box2)
+            points_in_box1 = box2.overlaps(box1)
+            # if Box1 is completely inside Box2, only add Box1 as overlap of Box2 --
+            # don't add Box2 to Box1 because when subtracting overlapping boxes this would make all of Box1 black
+            if points_in_box2 == 4:
+                overlaps[j].append(box1)
+            # similar to above
+            elif points_in_box1 == 4:
+                overlaps[i].append(box2)
+            # add as overlapping each other if not completely overlapping
+            elif points_in_box1 or points_in_box2:
+                overlaps[i].append(box2)
+                overlaps[j].append(box1)
+
+    return overlaps
+
+def find_balloon_ils(image, bounding_boxes):
+    """ Takes the binary image and the bounding_boxes to find some set in the middle
+        of cells to start ballooning. Returns a list of these initial level sets."""
+    image_working = deepcopy(image)
+    initial_level_sets = []
+    # find overlaps of bboxes
+    overlaps = get_bbox_overlaps(bounding_boxes)
+
+    for i in range(len(bounding_boxes)):
+        box = bounding_boxes[i]
+
+        # removes all other cells so only bright spots are cell in question
+        for overlap_box in overlaps[i]:
+            image_working[overlap_box.y1:overlap_box.y2 + 1, overlap_box.x1:overlap_box.x2 + 1] = 0
+
+        # creates an np.array image of just the current bbox
+        subimage = box.subimage(image_working)
+        # finds connected bright regions (foreground)
+        labels_mask = measure.label(subimage, connectivity=2)
+        # determines quantitative properties of each region of brightness
+        regions = measure.regionprops(labels_mask)
+        # sorts the regions, largest area to smallest
+        regions.sort(key=lambda x: x.area, reverse=True)
+        # take the region with largest area and find its center
+        y, x = regions[0].centroid                 # subimage ils bbox
+        y = int(y)
+        x = int(x)
+
+        # create a 2x2 ils in the center of the largest region (map back to original image size)
+        ils = np.zeros(image.shape, dtype=np.int8)
+        ils[box.y1 + y - 1:box.y1 + y + 1, box.x1 + x - 1:box.x2 + x + 1] = 1
+        initial_level_sets.append(ils)
+
+        # replaces subtracted cells
+        image_working = deepcopy(image)
+
+    return initial_level_sets
+
+def contour(image_binary, bounding_boxes):
+    """ Takes a binary image and corresponding bounding boxes and returns a list of
+        cell contours, [np.array]"""
+    contours = []
+    # finds areas inside cells to start ballooning
+    initial_level_sets = find_balloon_ils(image_binary, bounding_boxes)
+    image_binary = util.img_as_float(image_binary)
+
+    for i in range(len(bounding_boxes)):
+        box = bounding_boxes[i]
+        ils = initial_level_sets[i]
+        # computes the number of iterations for the contour method to grow --
+        # based on the size of the diagonal of the bounding box multiplied by a scalar
+        iterations = int(math.sqrt(box.width() ** 2 + box.height() ** 2) // 2.5)
+        # finds the contour and stores them
+        contours.append(segmentation.morphological_geodesic_active_contour(image_binary, iterations, init_level_set=ils, smoothing=1, balloon=1))
+
+    return contours
--- make_labeled_crops.py
+++ make_labeled_crops.py
@@ -5,113 +5,11 @@ import sys
 from PIL import Image
 from copy import deepcopy
 from os import listdir
+from classes import *
 
 TILE_OVERLAP = 2 # 2 -> 50% overlap, 3 -> 33% overlap, etc.
 TILE_SIZE = 416
 IMAGE_EXTENSIONS = (".tiff", ".tif", ".png", ".jpg", ".jpeg", ".gif")
-
-class Box:
-    def __init__(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
-
-    def width(self):
-        return self.x2 - self.x1
-
-    def height(self):
-        return self.y2 - self.y1
-
-    def center(self):
-        return self.x1 + self.width() / 2, self.y1 + self.height() / 2
-
-
-class Tile(Box):
-    def __init__(self, crop, x1, y1, x2, y2, filename):
-        """ crop:           The cropped PIL Image object.
-            x1, y1, x2, y2: The position of this tile in the larger image. """
-        super().__init__(x1, y1, x2, y2)
-        self.crop = crop
-
-        # This will store all the (potentially partial) bounding boxes that overlap this tile.
-        # Their coordinates are relative to this tile.
-        # (i.e. if a bounding box starts on the left edge of this tile, its x1 is 0)
-        self.bounding_boxes = []
-
-        # This will be used as a unique identifier for this crop.
-        self.filename = f"{filename}_{self.x1}_{self.y1}"
-
-    def add_bounding_box(self, box):
-        box = deepcopy(box)
-        box.x1 = max(box.x1, self.x1) - self.x1
-        box.y1 = max(box.y1, self.y1) - self.y1
-        box.x2 = min(box.x2, self.x2) - self.x1
-        box.y2 = min(box.y2, self.y2) - self.y1
-        self.bounding_boxes.append(box)
-
-    def to_relative_bounding_boxes(self):
-        for box in self.bounding_boxes:
-            box.to_relative(TILE_SIZE, TILE_SIZE)
-
-    def save(self, directory="."):
-        """ Saves this tile as a cropped image and an associated label file.
-            Note: self.bounding_boxes must all be in relative coords for YOLO to like the label output. """
-        self.crop.save(f"{directory}/{self.filename}.jpg", "JPEG", subsampling=0, quality=100)
-        
-        if self.bounding_boxes != []:
-            ofile = open(f"{directory}/{self.filename}.txt", "w")
-            for box in self.bounding_boxes:
-                ofile.write(f"{box.classification} {box.center()[0]} {box.center()[1]} {box.width()} {box.height()}\n")
-            ofile.close()
-
-
-class BoundingBox(Box):
-    def __init__(self, classification, x, y, width, height):
-        """ classification: A number representing a YOLO classification
-            x, y:           Floats between 0 and 1 representing the relative position of this box's center.
-            width, height:  Floats representing the relative dimensions of this box. """
-        super().__init__(x - width / 2, y - height / 2, x + width / 2, y + height / 2)
-        self.classification = int(classification)
-        self.in_px = False
-
-    def to_px(self, width, height):
-        assert not self.in_px
-        self.x1 *= width
-        self.x2 *= width
-        self.y1 *= height
-        self.y2 *= height
-        self.in_px = True
-
-    def to_relative(self, width, height):
-        assert self.in_px
-        self.x1 /= width
-        self.x2 /= width
-        self.y1 /= height
-        self.y2 /= height
-        self.in_px = False
-
-    def overlaps(self, box):
-        assert self.in_px
-
-        return int(self.x1) < int(box.x2) and int(box.x1) < int(self.x2) \
-           and int(self.y1) < int(box.y2) and int(box.y1) < int(self.y2)
-
-        
-def parse_yolo_input(label_file):
-    """ Reads from a yolo training file and returns a list of BoundingBox objects.
-        Also takes the labels' image so we can convert from relative to px. """
-    bounding_boxes = []
-    for line in label_file.readlines():
-        # Ignore comments
-        if "#" in line:
-            line = line[:line.index("#")]
-        if line.split() == []:
-            continue
-
-        bounding_boxes.append(BoundingBox(*map(float, line.split())))
-
-    return bounding_boxes
 
 def make_crops(img, filename):
     tiles = []
@@ -145,7 +43,7 @@ def make_labeled_crops(input_dir, output_dir):
             # Open the files
             img = Image.open(image_filename)
             labels = open(label_filename)
-            
+
             # Make the BoundingBox objects
             bounding_boxes = parse_yolo_input(labels)
 
