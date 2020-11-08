df3ae00f0950653df1e336e82c43da3010edfae2
Ben Kallus <bkallus@hamilton.edu>
2020-11-08 11:37:27 -0500

    new model

--- src/main_window.py
+++ src/main_window.py
@@ -107,6 +107,7 @@ class MainWindow(QMainWindow):
         self.MplWidget.remove_cell_bounding_boxes()
         self.MplWidget.draw_cell_network_edges(self.program_manager.bio_objs)
 
+        self.actionViewBoundingBoxes.setEnabled(True)
         self.actionSave.setEnabled(True)
         self.actionSaveAs.setEnabled(True)
         self.actionExportToGephi.setEnabled(True)
