b423502dede2f6a4dbcf5e683e80811560eee5a1
Ben Kallus <bkallus@hamilton.edu>
2020-11-10 11:01:39 -0500

    now dependencies actually install

--- mac-install.sh
+++ mac-install.sh
@@ -12,29 +12,36 @@ else
 fi
 
 # Install our brew dependencies
-brew install python3 gcc git pyqt5
+brew install gcc libomp
 # bsd make comes with the xcode tools that get installed alongside homebrew, and darknet doesn't use
 # any of the gnu make features, so we don't need to brew install make.
 
 # If this is being run from within this repo, then great! Otherwise, we should go clone the repo.
 # Maybe we should come up with a better way of checking whether we're in the repo than the directory name.
 if [ "$(basename $(pwd))" != "bacteria-networks" ]; then
-    git clone --depth 1 --recurse-submodules https://github.com/ahthomps/bacteria-networks.git
+    curl -L https://github.com/ahthomps/bacteria-networks/archive/master.zip -o bacteria-networks.zip
+    unzip bacteria-networks.zip
+    rm bacteria-networks.zip
+    mv bacteria-networks-master bacteria-networks
     cd bacteria-networks
+fi
+fi [ -z "$(ls darknet)" ]; then
+    rmdir darknet
+    curl -L https://github.com/AlexeyAB/darknet/archive/master.zip -o darknet.zip
+    unzip darknet.zip
+    rm darknet.zip
+    mv darknet_master darknet
 fi
 
 # Because Apple sucks, they symlink gcc to clang. So the version of gcc brew installs has to be installed as gcc-10.
 # When gcc 11 comes out, this will have to be updated.
-sed -i "" "s/gcc/gcc-10" darknet/Makefile
-sed -i "" "s/g++/g++-10" darknet/Makefile
+sed -i "" "s/gcc/gcc-10/" darknet/Makefile
+sed -i "" "s/g++/g++-10/" darknet/Makefile
 sed -i "" "s/OPENMP=0/OPENMP=1/" darknet/Makefile # Enables multicore support for running the neural network.
 sed -i "" "s/AVX=0/AVX=1/" darknet/Makefile # Enables vector instructions for running the neural network.
 
 # Install our pip dependencies
-pip3 install --user numpy matplotlib scikit-image networkx
-
-# We do this last because otherwise some of the pip packages will try to build using openmp and get blocked because clang doesn't support it.
-brew install libomp
+pip3 install --user numpy matplotlib scipy scikit-image networkx pyqt5
 
 # Now, we need to get the model. We need a place to host it that isn't Google Drive so it can be downloaded from this script.
 # Ideally, we'd get a cs server to host it, and we can host this script there too.
