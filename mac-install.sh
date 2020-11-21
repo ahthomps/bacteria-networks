103b0c183850e206660112c251077e30e4c4d161
Ben Kallus <bkallus@hamilton.edu>
2020-11-21 17:54:52 -0500

    quotes in bash are really annoying

--- mac-install.sh
+++ mac-install.sh
@@ -5,7 +5,7 @@
 
 # The pip dependencies. If you get a pip error, you might want to add version numbers to these.
 PIP_DEPENDENCIES="scikit-image numpy matplotlib scipy networkx pyqt5 Pillow"
-SHORTCUT_PATH="~/Desktop/JAB" # they probably don't already have something named JAB on their desktop...
+SHORTCUT_PATH=~/Desktop/JAB # they probably don't already have something named JAB on their desktop...
 
 # If homebrew isn't installed, then install it.
 which -s brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -63,5 +63,5 @@ cd .. &&
 rm -rf bacteria-networks-model-master
 
 touch $SHORTCUT_PATH
-echo -e "#!/bin/bash\nchmod +x ~/bacteria-networks/run.py\n~bacteria-networks/run.py" > $SHORTCUT_PATH
-chmod +x $SHORTCUT_PATH
+echo -e "#!/bin/bash\nchmod +x ~/bacteria-networks/run.py\n~bacteria-networks/run.py" > "$SHORTCUT_PATH"
+chmod +x "$SHORTCUT_PATH"
