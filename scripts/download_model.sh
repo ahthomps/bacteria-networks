617f25ea285ab6f8d317b9923f328d443ca38a70
Ben Kallus <bkallus@hamilton.edu>
2020-11-22 23:20:51 -0500

    updated model downloading script

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -5,7 +5,7 @@ curl -L https://github.com/kenballus/bacteria-networks-model/archive/master.zip 
 unzip bacteria-networks-model.zip
 rm bacteria-networks-model.zip
 cd bacteria-networks-model-master
-cat *.part > model_5.weights
-mv model_5.weights ../../models/model_5
+cat *.part > model_6.weights
+mv model_6.weights ../../models/model_6
 cd ..
 rm -rf bacteria-networks-model-master
